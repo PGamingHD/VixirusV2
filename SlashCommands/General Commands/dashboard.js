2889f31268b18f46d646e540191e3eda0a927fb5
PGamingHD <pontus.2003@hotmail.com>
2022-12-20 15:39:18 +0100

    Dashboard cmd!
    Dashboard cmd!

---
+++ SlashCommands/General Commands/dashboard.js
@@ -0,0 +1,36 @@
+const {
+    Client,
+    CommandInteraction,
+    MessageEmbed,
+    MessageActionRow,
+    MessageButton,
+    EmbedBuilder
+} = require('discord.js');
+const ee = require('../../botconfig/embed.json');
+const emoji = require('../../botconfig/embed.json')
+const prettyMilliseconds = require('pretty-ms');
+const config = require('../../botconfig/config.json');
+const {
+    languageControl,
+    stringTemplateParser
+} = require("../../handler/functions");
+
+module.exports = {
+    name: 'dashboard',
+    description: 'Interested in controlling me from my dashboard? Use this command to get the dashboard link.',
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async (client, interaction, args) => {
+        return interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                .setColor(ee.color)
+                .setTitle('Dashboard Link')
+                .setDescription('**!COMING SOON!**')
+            ]
+        })
+    }
+}
