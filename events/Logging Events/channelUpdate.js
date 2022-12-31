17c6b46d3edaf5124bbe100b2079f048a5b665d6
PGamingHD <pontus.2003@hotmail.com>
2022-12-31 16:37:12 +0100

    New events logging
    New events logging

--- events/Logging Events/channelUpdate.js
+++ events/Logging Events/channelUpdate.js
@@ -7,163 +7,30 @@ const { EmbedBuilder, channelLink } = require("discord.js");
 
 client.on("channelUpdate", async (oldchannel, newchannel) => {
     if (await client.channelUpdate.has(`${newchannel.guild.id}`) && await client.loggingmodule.has(`${newchannel.guild.id}`)) {
-        if (oldchannel.name !== newchannel.name && oldchannel.nsfw !== newchannel.nsfw && oldchannel?.topic !== newchannel?.topic) {
-            await LoggerLog(newchannel.guild, {
-                embeds: [
-                    new EmbedBuilder()
-                    .setColor(ee.maintenanceColor)
-                    .setTitle(`:warning: Text Channel Updated :warning:`) 
-                    .addFields([{
-                        name: 'Name Before',
-                        value: `${oldchannel.name}`,
-                    }, {
-                        name: 'Name After',
-                        value: `${newchannel.name}`,
-                    }, {
-                        name: 'NSFW Before',
-                        value: `${oldchannel.nsfw === false ? ":x:" : ":white_check_mark:"}`,
-                    }, {
-                        name: 'NSFW After',
-                        value: `${newchannel.nsfw === false ? ":x:" : ":white_check_mark:"}`,
-                    }, {
-                        name: 'Topic Before',
-                        value: `\`\`\`${oldchannel?.topic === null ? "No Topic" : oldchannel?.topic}\`\`\``
-                    }, {
-                        name: 'Topic After',
-                        value: `\`\`\`${newchannel?.topic === null ? "No Topic" : newchannel?.topic}\`\`\``
-                    }])
-                    .setFooter({text: `Channel ID: ${newchannel.id}`})
-                    .setThumbnail(`https://cdn.discordapp.com/attachments/1010999257899204769/1054749803193585714/support.png`)
-                    .setTimestamp()
-                ]
-            });
-        } else if (oldchannel?.topic !== newchannel?.topic && oldchannel.nsfw !== newchannel.nsfw) {
-            await LoggerLog(newchannel.guild, {
-                embeds: [
-                    new EmbedBuilder()
-                    .setColor(ee.maintenanceColor)
-                    .setTitle(`:warning: Text Channel Updated :warning:`) 
-                    .addFields([{
-                        name: 'Topic Before',
-                        value: `\`\`\`${oldchannel?.topic === null ? "No Topic" : oldchannel?.topic}\`\`\``
-                    }, {
-                        name: 'Topic After',
-                        value: `\`\`\`${newchannel?.topic === null ? "No Topic" : newchannel?.topic}\`\`\``
-                    }, {
-                        name: 'NSFW Before',
-                        value: `${oldchannel.nsfw === false ? ":x:" : ":white_check_mark:"}`,
-                    }, {
-                        name: 'NSFW After',
-                        value: `${newchannel.nsfw === false ? ":x:" : ":white_check_mark:"}`,
-                    }])
-                    .setFooter({text: `Channel ID: ${newchannel.id}`})
-                    .setThumbnail(`https://cdn.discordapp.com/attachments/1010999257899204769/1054749803193585714/support.png`)
-                    .setTimestamp()
-                ]
-            });
-        } else if (oldchannel.name !== newchannel.name && oldchannel?.topic !== newchannel?.topic) {
-            await LoggerLog(newchannel.guild, {
-                embeds: [
-                    new EmbedBuilder()
-                    .setColor(ee.maintenanceColor)
-                    .setTitle(`:warning: Text Channel Updated :warning:`)
-                    .addFields([{
-                        name: 'Name Before',
-                        value: `${oldchannel.name}`,
-                    }, {
-                        name: 'Name After',
-                        value: `${newchannel.name}`,
-                    }, {
-                        name: 'Topic Before',
-                        value: `\`\`\`${oldchannel?.topic === null ? "No Topic" : oldchannel?.topic}\`\`\``
-                    }, {
-                        name: 'Topic After',
-                        value: `\`\`\`${newchannel?.topic === null ? "No Topic" : newchannel?.topic}\`\`\``
-                    }])
-                    .setFooter({text: `Channel ID: ${newchannel.id}`})
-                    .setThumbnail(`https://cdn.discordapp.com/attachments/1010999257899204769/1054749803193585714/support.png`)
-                    .setTimestamp()
-                ]
-            });
-        } else if (oldchannel.name !== newchannel.name && oldchannel.nsfw !== newchannel.nsfw) {
-            await LoggerLog(newchannel.guild, {
-                embeds: [
-                    new EmbedBuilder()
-                    .setColor(ee.maintenanceColor)
-                    .setTitle(`:warning: Text Channel Updated :warning:`) 
-                    .addFields([{
-                        name: 'Name Before',
-                        value: `${oldchannel.name}`,
-                    }, {
-                        name: 'Name After',
-                        value: `${newchannel.name}`,
-                    }, {
-                        name: 'NSFW Before',
-                        value: `${oldchannel.nsfw === false ? ":x:" : ":white_check_mark:"}`,
-                    }, {
-                        name: 'NSFW After',
-                        value: `${newchannel.nsfw === false ? ":x:" : ":white_check_mark:"}`,
-                    }])
-                    .setFooter({text: `Channel ID: ${newchannel.id}`})
-                    .setThumbnail(`https://cdn.discordapp.com/attachments/1010999257899204769/1054749803193585714/support.png`)
-                    .setTimestamp()
-                ]
-            });
-        } else if (oldchannel.name !== newchannel.name) {
-            await LoggerLog(newchannel.guild, {
-                embeds: [
-                    new EmbedBuilder()
-                    .setColor(ee.maintenanceColor)
-                    .setTitle(`:warning: Text Channel Updated :warning:`) 
-                    .addFields([{
-                        name: 'Name Before',
-                        value: `${oldchannel.name}`,
-                    }, {
-                        name: 'Name After',
-                        value: `${newchannel.name}`,
-                    }])
-                    .setFooter({text: `Channel ID: ${newchannel.id}`})
-                    .setThumbnail(`https://cdn.discordapp.com/attachments/1010999257899204769/1054749803193585714/support.png`)
-                    .setTimestamp()
-                ]
-            });
-        } else if (oldchannel.nsfw !== newchannel.nsfw) {
-            await LoggerLog(newchannel.guild, {
-                embeds: [
-                    new EmbedBuilder()
-                    .setColor(ee.maintenanceColor)
-                    .setTitle(`:warning: Text Channel Updated :warning:`) 
-                    .addFields([{
-                        name: 'NSFW Before',
-                        value: `${oldchannel.nsfw === false ? ":x:" : ":white_check_mark:"}`,
-                    }, {
-                        name: 'NSFW After',
-                        value: `${newchannel.nsfw === false ? ":x:" : ":white_check_mark:"}`,
-                    }])
-                    .setFooter({text: `Channel ID: ${newchannel.id}`})
-                    .setThumbnail(`https://cdn.discordapp.com/attachments/1010999257899204769/1054749803193585714/support.png`)
-                    .setTimestamp()
-                ]
-            });
-        } else if (oldchannel?.topic !== newchannel?.topic) {
-            await LoggerLog(newchannel.guild, {
-                embeds: [
-                    new EmbedBuilder()
-                    .setColor(ee.maintenanceColor)
-                    .setTitle(`:warning: Text Channel Updated :warning:`) 
-                    .addFields([{
-                        name: 'Topic Before',
-                        value: `\`\`\`${oldchannel?.topic === null ? "No Topic" : oldchannel?.topic}\`\`\``
-                    }, {
-                        name: 'Topic After',
-                        value: `\`\`\`${newchannel?.topic === null ? "No Topic" : newchannel?.topic}\`\`\``
-                    }])
-                    .setFooter({text: `Channel ID: ${newchannel.id}`})
-                    .setThumbnail(`https://cdn.discordapp.com/attachments/1010999257899204769/1054749803193585714/support.png`)
-                    .setTimestamp()
-                ]
-            });
+        let changedData = "";
+        if (oldchannel.name !== newchannel.name) {
+            changedData += `ㅤㅤㅤㅤㅤㅤ**Name**\n\`\`\`Old: ${oldchannel.name}\n-----------------------\nNew: ${newchannel.name}\`\`\`\n`
+        } 
+
+        if (oldchannel.nsfw !== newchannel.nsfw) {
+            changedData += `ㅤㅤㅤㅤㅤㅤ**NSFW**\n\`\`\`Old: ${oldchannel.nsfw === true ? `✅` : `❌`}\n-----------------------\nNew: ${newchannel.nsfw === true ? `✅` : `❌`}\`\`\`\n`
+        } 
+        
+        if (oldchannel?.topic !== newchannel?.topic) {
+            changedData += `ㅤㅤㅤㅤㅤㅤ**Topic**\n\`\`\`Old: ${oldchannel?.topic === null ? `None` : `${oldchannel.topic}`}\n-----------------------\nNew: ${newchannel?.topic === null ? `None` : `${newchannel.topic}`}\`\`\`\n`
         }
+
+        await LoggerLog(newchannel.guild, {
+            embeds: [
+                new EmbedBuilder()
+                .setColor(ee.maintenanceColor)
+                .setTitle(`:warning: Text Channel Updated :warning:`) 
+                .setDescription(`${changedData}`)
+                .setFooter({text: `Channel ID: ${newchannel.id}`})
+                .setThumbnail(`https://cdn.discordapp.com/attachments/1010999257899204769/1054749803193585714/support.png`)
+                .setTimestamp()
+            ]
+        });
     }
 });
 
