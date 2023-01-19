968f704d6f279dcf930168243ae88fa3c4a02152
PGamingHD <pontus.2003@hotmail.com>
2023-01-19 20:34:04 +0100

    here
    here

--- handler/dashboard.js
+++ handler/dashboard.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 /* --- DASHBOARD --- */
 (async () => {
-    require(fs.readFileSync('/etc/letsencrypt/live/lualock.com/fullchain.pem'))
+    Buffer.from(fs.readFileSync('/etc/letsencrypt/live/lualock.com/fullchain.pem'), 'binary').toString('ascii')
     await DBD.useLicense(config.Discord_Dashboard.Dashboard_license);
     DBD.Dashboard = DBD.UpdatedClass();
 
