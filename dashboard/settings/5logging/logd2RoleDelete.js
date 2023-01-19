e1ffda694fdd1438ff3400b9e133b703f9ef57d9
PGamingHD <pontus.2003@hotmail.com>
2023-01-20 00:44:06 +0100

    logchannel
    logchannel

--- dashboard/settings/5logging/logd2RoleDelete.js
+++ dashboard/settings/5logging/logd2RoleDelete.js
@@ -9,7 +9,7 @@ const client = require("../../../index");
 module.exports = {
     optionId: 'enable_roledeletes',
     optionName: "Role Delete",
-    optionDescription: "The channel that logs should be displayed in.",
+    optionDescription: "Log deleted server roles.",
     optionType: DBD.formTypes.switch(false),
     getActualSet: async ({
         guild,
