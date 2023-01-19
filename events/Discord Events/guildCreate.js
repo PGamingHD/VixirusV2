f60c26b5bcdaa98b9c1f087d2dbfeb62f07fe57e
PGamingHD <pontus.2003@hotmail.com>
2023-01-19 23:40:03 +0100

    send msg
    send msg

--- events/Discord Events/guildCreate.js
+++ events/Discord Events/guildCreate.js
@@ -72,16 +72,21 @@ client.on("guildCreate", async (guild) => {
     }
 
     try {
-        const ch = guild.channels.cache.find(channel => channel.type === ChannelType.GuildText && channel.permissionsFor(guild.members.me).has(PermissionFlagsBits.SendMessages) && channel.permissionsFor(guild.members.me).has(PermissionFlagsBits.EmbedLinks));
+        const ch = guild.channels.cache.find(channel => channel.type === ChannelType.GuildText && channel.permissionsFor(guild.members.me).has(PermissionFlagsBits.SendMessages));
 
         if (!ch) return;
 
         return await ch.send({
-            content: await languageControl(guild, 'WELCOME_MSG')
+            embeds: [
+                new EmbedBuilder()
+                .setColor(ee.hidden)
+                .setThumbnail('https://cdn.discordapp.com/attachments/1063980215505788958/1063980431931883560/ad2e4d6e7b90ca6005a5038e22b099cc.png')
+                .setDescription(`🛡️ ***Server Protected by VixirusV2*** 🛡️\n\n\`• How is your server protected?\`\n*- With our system, if an earlier punished user joins your server you will be alerted with the reason and can from there decide to punish the user in your own server aswell.*\n\n\`• Is this system really reliable?\`\n*- Not only is this system armed with a strong logging system, the system is also a very good defense against known raiders / known rule breakers!*\n\n\`• Is this system configurable?\`\n*- This system has large configurability, configure the system to your own liking through our dashboard [here](https://lualock.com)!*\n\n\`• Are we allowed to suggest new features?\`\n*- Yes, we really appreciate new suggestions so that we can improve the product. Please join our support server and suggest new features all you want! :)*\n\n\`• Where can I get support for this product?\`\n*- The support server can be found [here](${config.Discord_Links.Support_Server}), don't hesitate to join and ask questions.*\n\n🛡️ ***Server Protected by VixirusV2*** 🛡️`)
+                .setFooter({text: 'by PG Technologies™'})
+                .setTimestamp()
+            ]
         });
-    } catch (error) {
-        console.log(error)
-    }
+    } catch {}
 
     await pool.release();
 });
