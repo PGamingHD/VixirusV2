0aabba9e6bd0a4bcdbd559a11cc5f63d4f7b1d20
PGamingHD <pontus.2003@hotmail.com>
2023-01-20 00:35:34 +0100

    ye
    he

--- dashboard/settings/5logging/test.js
+++ dashboard/settings/5logging/test.js
@@ -1,8 +1,6 @@
 const DBD = require('discord-dashboard');
+const { ChannelType } = require('discord.js');
 const getPool = require("../../../handler/database");
-const {
-    ChannelType
-} = require('discord.js');
 const {
     writeError,
     guildHasData
@@ -10,17 +8,16 @@ const {
 const client = require("../../../index");
 
 module.exports = {
-    optionId: 'logchannel',
-    optionName: "Logging Channel [BROKEN, FIX SOON]",
-    optionDescription: "The channel that logs should be displayed in.",
+    optionId: 'log_channels',
+    optionName: "Moderation Channel",
+    optionDescription: "Change the Moderation Channel, log all moderation command usages into this channel.",
     optionType: DBD.formTypes.channelsSelect(false, [ChannelType.GuildText], true, false),
     getActualSet: async ({
-        guild,
-        user
+        guild,user
     }) => {
         try {
-            return await client.cachedLoggingChannels.get(`${guild.id}`);
-        } catch (error) {
+            return await client.cachedModLogs.get(`${guild.id}`);
+        } catch(error) {
             return writeError(error, guild);
         }
     },
@@ -31,15 +28,15 @@ module.exports = {
         const pool = await getPool().getConnection();
 
         await guildHasData(guild, pool);
-
+        
         try {
+            if (newData.length === 0) return {error: 'You may not return no channels'};
+            if (newData[0] === "" || newData[0] === undefined) return {error: 'No channels were found, please select a channel first'};
             const [guildData, guildRows] = await pool.query(`SELECT * FROM guild_data WHERE data_ServerId = ${guild.id}`);
-            console.log(guildData)
             if (guildData.length === 0) return;
-            if (newData === "") newData = "0";
-            await pool.query(`UPDATE guild_data SET data_logchannel = '${newData}' WHERE data_ServerId = ${guild.id}`);
+            await pool.query(`UPDATE guild_data SET data_modlogs = '${newData}' WHERE data_ServerId = ${guild.id}`);
 
-            await client.cachedLoggingChannels.set(`${guild.id}`, newData);
+            await client.cachedModLogs.set(`${guild.id}`, newData);
 
             return await pool.release();
         } catch (error) {
