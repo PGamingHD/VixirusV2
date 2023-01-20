829802d6aae74b359810d9d16fc9e29e67b8a86a
PGamingHD <pontus.2003@hotmail.com>
2023-01-20 18:32:24 +0100

    Added AI in fun cmd!
    Added AI in fun cmd!

---
+++ SlashCommands/Fun Commands/ai.js
@@ -0,0 +1,61 @@
+const {
+    Client,
+    CommandInteraction,
+    MessageEmbed,
+    MessageActionRow,
+    MessageButton,
+    EmbedBuilder,
+    ApplicationCommandOptionType
+} = require('discord.js');
+const ee = require('../../botconfig/embed.json');
+const emoji = require('../../botconfig/embed.json')
+const prettyMilliseconds = require('pretty-ms');
+const config = require('../../botconfig/config.json');
+const {
+    languageControl,
+    stringTemplateParser
+} = require("../../handler/functions");
+require('dotenv').config();
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+module.exports = {
+    name: 'ai',
+    description: 'Ask the OpenAI anything, provided by OpenAI!',
+    options: [{
+        name: 'question',
+        description: 'The question you wish to ask the AI',
+        type: ApplicationCommandOptionType.String,
+        required: true
+    }],
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async (client, interaction, args) => {
+        const question = interaction.options.getString('question');
+        
+        try {
+            const completion = await openai.createCompletion({
+              model: "text-davinci-002",
+              prompt: question,
+            });
+            
+            return interaction.reply({
+                embeds: [
+                    new EmbedBuilder()
+                    .setColor(ee.color)
+                    .setTitle(`:robot: AI Response :robot:`)
+                    .setDescription(`\`\`\`${completion.data.choices[0].text}\`\`\``)
+                    .setTimestamp()
+                ]
+            })
+          } catch (error) {
+            console.log(error)
+          }
+    }
+}
