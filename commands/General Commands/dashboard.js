d36bcd9ef36f4b2109a49a6de64e8f107036536f
PGamingHD <pontus.2003@hotmail.com>
2023-01-20 13:44:30 +0100

    dashboard
    dashboard

--- commands/General Commands/dashboard.js
+++ commands/General Commands/dashboard.js
@@ -29,7 +29,7 @@ module.exports = {
                 new EmbedBuilder()
                 .setColor(ee.color)
                 .setTitle('Dashboard Link')
-                .setDescription('*Configure your bot trough the [dashboard](https://vixirus.com/)!*')
+                .setDescription(`*Configure your bot trough the [dashboard](${config.Discord_Dashboard.Dashboard_domain})!*`)
             ]
         })
     },
